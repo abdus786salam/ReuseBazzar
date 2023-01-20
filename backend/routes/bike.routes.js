4d84da9381e88c00d478887863af23e187b3784b
vinitvinay <vinaymasai1998@gmail.com>
2023-01-20 12:08:49 +0530

    get

--- backend/routes/bike.routes.js
+++ backend/routes/bike.routes.js
@@ -6,7 +6,14 @@ const { Bikemodel } = require("../models/Bikes.model");
 
 const storage = multer.memoryStorage();
 const upload = multer({ storage });
-
+bikeRouter.get("/", async (req, res) => {
+  try {
+    const bikes = await Bikemodel.find();
+    res.send(bikes);
+  } catch (error) {
+    res.send({ msg: "Somethinf went wrong" });
+  }
+});
 bikeRouter.post("/post", upload.single("image"), async (req, res) => {
   const new_bike = new Bikemodel({
     brand: req.body.brand,
