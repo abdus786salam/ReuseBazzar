cde97265bd167fab245872c886d0e780b7711160
Ankita <ankitathakurankita7007@gmail.com>
2023-01-20 12:12:49 +0000

    Add

--- quickr/src/App.js
+++ quickr/src/App.js
@@ -1,18 +1,14 @@
 
 
-import Responsive from './Components/HomePage/Adds';
-import { BuySell } from './Components/HomePage/buySell';
-import { MostPopular } from './Components/HomePage/MostPopularProduct';
+
 import AllRoutes from './higherOrderFunc/AllRoutes';
 
 function App() {
   return (
     <div>
-      {/* <AllRoutes /> */}
+      <AllRoutes />
       
-     <MostPopular/>
-     <BuySell/>
-     <Responsive/>
+     
     </div>
   );
 }
